36e72e7cb6029fc2ca4450797916941c643e2b6d
Karsten Ohme <k_o_@users.sourceforge.net>
2026-02-08 20:25:45 +0100

    adding hash

---
+++ README.md
@@ -0,0 +1,48 @@
+# Summary
+
+[vcpkg registry](https://vcpkg.io) for [GlobalPlatform library](https://github.com/kaoh/globalplatform).
+
+# Version Update
+
+Update the GlobalPlatform library version:
+
+~~~shell
+nano ports/globalplatform/vcpkg.json
+~~~
+
+Set the version accordingly:
+
+`"version-string": "8.0.0",`
+
+Update the tag and set the `SHA512` to 0:
+
+~~~shell
+nano ports/globalplatform/portfile.cmake 
+~~~
+
+Set the tag:
+
+`REF <tag>`
+
+Copy the failed hash value from the `install` command:
+
+~~~shell
+~/vcpkg/vcpkg install globalplatform --overlay-ports=./ports
+~~~
+
+You will see a message like:
+
+> error: failing download because the expected SHA512 was all zeros, please change the expected SHA512 to: 0a66f9254390fbc27f048a76f96f9a9f858a903f55fbc67c42dd083a55b8537e6a7c436d76d67d71d327b9dd153b0832b5b46dcacce2284e9ca6bd5108d576ab
+
+Replace the `SHA512` value:
+
+~~~shell
+nano ports/globalplatform/portfile.cmake
+~~~
+
+~~~shell
+~/vcpkg/vcpkg x-add-version globalplatform --x-builtin-ports-root=./ports --x-builtin-registry-versions-dir=./versions
+
+~/vcpkg/vcpkg x-add-version globalplatform --vcpkg-root=. --overwrite-version
+
+~~~
--- ports/globalplatform/portfile.cmake
+++ ports/globalplatform/portfile.cmake
@@ -1,19 +1,9 @@
-set(SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../globalplatform")
-
-#include("${VCPKG_ROOT_DIR}/scripts/cmake/vcpkg_cmake_configure.cmake")
-#include("${VCPKG_ROOT_DIR}/scripts/cmake/vcpkg_cmake_install.cmake")
-#include("${VCPKG_ROOT_DIR}/scripts/cmake/vcpkg_cmake_config_fixup.cmake")
-
-message(STATUS "SOURCE_PATH = ${SOURCE_PATH}")
-
-#include("${VCPKG_ROOT_DIR}/scripts/cmake/vcpkg_cmake.cmake")
-
-#vcpkg_from_github(
-#    OUT_SOURCE_PATH SOURCE_PATH
-#    REPO kaoh/globalplatform
-#    REF master
-#    SHA512 0
-#)
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO kaoh/globalplatform
+    REF packaging
+    SHA512 0a66f9254390fbc27f048a76f96f9a9f858a903f55fbc67c42dd083a55b8537e6a7c436d76d67d71d327b9dd153b0832b5b46dcacce2284e9ca6bd5108d576ab
+)
 
 # Map vcpkg linkage -> your STATIC option
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
@@ -46,7 +36,6 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/cmake")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-# vcpkg ports usually don't ship share/doc
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/doc")
 
 vcpkg_install_copyright(FILE_LIST
